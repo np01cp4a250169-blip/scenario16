c5e83c99493279be7db603c81ed9a92b67e9c8a5
np01cp4a250169-blip <np01cp4a250169@islingtoncollege.edu.np>
2026-02-25 20:47:35 +0545

    Create Task1.java

---
+++ Week16/Week16/Week16/Task1.java
@@ -0,0 +1,32 @@
+package Week16;
+import java.util.Scanner;
+
+
+/**
+ * Write a description of class Task1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Task1
+{
+    public static void main (String[]args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter 1st string");
+        String s1 = input.next();
+        
+        System.out.println("Enter 2nd string");
+        String s2 = input.next();
+        
+        System.out.println("Concatination:"+s1.concat(s2));
+        
+        System.out.println("Enter 3rd string");
+        String s3 = input.next();
+        
+        System.out.println("equals:"+s1.concat(s2).equals(s3));
+        
+  
+    }
+    
+}
